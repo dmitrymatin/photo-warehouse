a33867950591e9abab4cdb0a77073bcd4bbae234
Dmitry Matin <super.dmitry320@yandex.ru>
2021-06-11 17:41:27 +0300

    Add Identity pages

---
+++ src/PhotoWarehouse/PhotoWarehouseApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,23 @@
+//using System;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.UI;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using PhotoWarehouse.Data;
+//using PhotoWarehouse.Domain.Users;
+
+//[assembly: HostingStartup(typeof(PhotoWarehouseApp.Areas.Identity.IdentityHostingStartup))]
+//namespace PhotoWarehouseApp.Areas.Identity
+//{
+//    public class IdentityHostingStartup : IHostingStartup
+//    {
+//        public void Configure(IWebHostBuilder builder)
+//        {
+//            builder.ConfigureServices((context, services) =>
+//            {
+//            });
+//        }
+//    }
+//}
