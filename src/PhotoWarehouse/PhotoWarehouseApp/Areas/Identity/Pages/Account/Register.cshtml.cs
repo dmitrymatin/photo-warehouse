c85d425dba07c476e1ac473f79e7fca2d4ea4104
Dmitry Matin <super.dmitry320@yandex.ru>
2021-06-12 01:05:33 +0300

    Initialise DateJoined field on registraion

--- src/PhotoWarehouse/PhotoWarehouseApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/PhotoWarehouse/PhotoWarehouseApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -75,7 +75,7 @@ namespace PhotoWarehouseApp.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, DateJoined = DateTimeOffset.UtcNow };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
