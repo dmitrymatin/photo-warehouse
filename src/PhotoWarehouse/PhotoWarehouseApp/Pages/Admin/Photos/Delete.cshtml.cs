bb659a7575ecce03aefe01573c8f5c4f062d16fb
Dmitry Matin <super.dmitry320@yandex.ru>
2021-06-14 23:00:14 +0300

    Add Photos CRUD

---
+++ src/PhotoWarehouse/PhotoWarehouseApp/Pages/Admin/Photos/Delete.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PhotoWarehouse.Data;
+using PhotoWarehouse.Domain.Photos;
+
+namespace PhotoWarehouseApp.Pages.Admin.Photos
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly PhotoWarehouse.Data.ApplicationDbContext _context;
+
+        public DeleteModel(PhotoWarehouse.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Photo Photo { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Photo = await _context.Photos
+                .Include(p => p.Category).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Photo == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Photo = await _context.Photos.FindAsync(id);
+
+            if (Photo != null)
+            {
+                _context.Photos.Remove(Photo);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ src/PhotoWarehouse/PhotoWarehouseApp/Pages/Admin/Photos/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PhotoWarehouse.Data;
+using PhotoWarehouse.Domain.Photos;
+
+namespace PhotoWarehouseApp.Pages.Admin.Photos
+{
+    public class EditModel : PageModel
+    {
+        private readonly PhotoWarehouse.Data.ApplicationDbContext _context;
+
+        public EditModel(PhotoWarehouse.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Photo Photo { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Photo = await _context.Photos
+                .Include(p => p.Category).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Photo == null)
+            {
+                return NotFound();
+            }
+           ViewData["CategoryId"] = new SelectList(_context.PhotoCategories, "Id", "Id");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Photo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PhotoExists(Photo.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool PhotoExists(int id)
+        {
+            return _context.Photos.Any(e => e.Id == id);
+        }
+    }
+}
