4bd08db035c6e377f1351ed9d50f1e6be588b703
Dmitry Matin <super.dmitry320@yandex.ru>
2021-06-14 23:00:38 +0300

    Add initial version for creating Photo

---
+++ src/PhotoWarehouse/PhotoWarehouseApp/Pages/Admin/Photos/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PhotoWarehouse.Data;
+using PhotoWarehouse.Domain.Photos;
+
+namespace PhotoWarehouseApp.Pages.Admin.Photos
+{
+    public class CreateModel : PageModel
+    {
+        private readonly PhotoWarehouse.Data.ApplicationDbContext _context;
+
+        public CreateModel(PhotoWarehouse.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["Category"] = new SelectList(_context.PhotoCategories, "Id", "Name");
+            return Page();
+        }
+
+        [BindProperty]
+        public Photo Photo { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Photo.InitialUploadDate = DateTimeOffset.UtcNow;
+            _context.Photos.Add(Photo);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ src/PhotoWarehouse/PhotoWarehouseApp/Pages/Admin/Photos/CreateMultiple.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PhotoWarehouse.Data;
+using PhotoWarehouse.Domain.Photos;
+
+namespace PhotoWarehouseApp.Pages.Admin.Photos
+{
+    public class CreateMultipleModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateMultipleModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.PhotoCategories, "Id", "Name");
+            return Page();
+        }
+
+        [BindProperty]
+        public Photo Photo { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Photos.Add(Photo);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
