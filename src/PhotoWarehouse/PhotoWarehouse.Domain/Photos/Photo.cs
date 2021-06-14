2bacb58aaf6002aedffa05bbf4be0b3bbb399dde
Dmitry Matin <super.dmitry320@yandex.ru>
2021-06-14 22:59:34 +0300

    Add some Display(Name=) attributes

--- src/PhotoWarehouse/PhotoWarehouse.Domain/Photos/Photo.cs
+++ src/PhotoWarehouse/PhotoWarehouse.Domain/Photos/Photo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhotoWarehouse.Domain.Photos
 {
@@ -8,7 +9,11 @@ namespace PhotoWarehouse.Domain.Photos
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        [Display(Name = "Initial upload date")]
         public DateTimeOffset InitialUploadDate { get; set; }
+
+        [Display(Name = "Date Taken")]
         public DateTimeOffset DateTaken { get; set; }
 
         public PhotoCategory Category { get; set; }
