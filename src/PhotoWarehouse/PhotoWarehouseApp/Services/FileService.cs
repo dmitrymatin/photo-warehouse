2fa0b51e1506973033430d3b46e9e81e2d02b810
Dmitry Matin <super.dmitry320@yandex.ru>
2021-06-15 15:29:01 +0300

    Add check IsImage() service and File helper services

---
+++ src/PhotoWarehouse/PhotoWarehouseApp/Services/FileService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace PhotoWarehouseApp.Services
+{
+    public class FileService
+    {
+        public static string EnsureCorrectPathAndFileName(
+            IWebHostEnvironment webHostEnvironment, 
+            IConfiguration configuration,
+            string filename
+            )
+        {
+            filename = filename.Trim('"');
+            if (filename.Contains("\\"))
+                filename = filename.Substring(filename.LastIndexOf("\\") + 1);
+
+            string uploadsFolder = configuration["UploadsFolder"];
+
+            return $"{webHostEnvironment.WebRootPath}\\{uploadsFolder}\\{filename}";
+        }
+
+        public static string GetExtension(string path)
+        {
+            return System.IO.Path.GetExtension(path);
+        }
+    }
+}
---
+++ src/PhotoWarehouse/PhotoWarehouseApp/Services/FormFileExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace PhotoWarehouseApp.Services
+{
+    public static class FormFileExtensions
+    {
+
+        public static bool IsImage(this IFormFile formFile, string extension)
+        {
+            //  Check the image mime types
+            string contentType = formFile.ContentType.ToLower();
+            if (contentType != "image/jpg" &&
+                        contentType != "image/jpeg" &&
+                        contentType != "image/pjpeg" &&
+                        contentType != "image/gif" &&
+                        contentType != "image/x-png" &&
+                        contentType != "image/png")
+            {
+                return false;
+            }
+
+            //  Check the image extension
+            extension = extension.ToLower();
+            if (extension != ".jpg"
+                && extension != ".png"
+                && extension != ".gif"
+                && extension != ".jpeg")
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
