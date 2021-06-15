9778fe96ab3b523dd005bec79b722337debab882
Dmitry Matin <super.dmitry320@yandex.ru>
2021-06-15 15:29:15 +0300

    Add initial version of IPhotoRepository

---
+++ src/PhotoWarehouse/PhotoWarehouse.Data/Repositories/IPhotoRepository.cs
@@ -0,0 +1,40 @@
+﻿using PhotoWarehouse.Domain.Photos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoWarehouse.Data.Repositories
+{
+    public interface IPhotoRepository
+    {
+        int Commit();
+        void Add(Photo photo);
+        Task AddPhotoItemAsync(Stream stream, PhotoItem photoItem, string path);
+    }
+
+    public class SQLPhotoRepository : IPhotoRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public void Add(Photo photo)
+        {
+            _context.Add(photo);
+        }
+
+        public async Task AddPhotoItemAsync(Stream stream, PhotoItem photoItem, string path)
+        {
+            using (FileStream output = File.Create(path))
+            {
+                await stream.CopyToAsync(output);
+            }
+        }
+
+        public int Commit()
+        {
+            return _context.SaveChanges();
+        }
+    }
+}
