8fd7268ac01f20d71f5ea3d3e4d8ced38485da9f
Dmitry Matin <super.dmitry320@yandex.ru>
2021-06-22 19:41:14 +0300

    Make initial changes to handle categories menu

--- src/PhotoWarehouse/PhotoWarehouseApp/wwwroot/js/site.js
+++ src/PhotoWarehouse/PhotoWarehouseApp/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿const menuToggle = document.querySelector('#categoriesMenuToggle');
+menuToggle.addEventListener('click', () => {
+    const menu = document.querySelector('#expMenu');
+    menu.classList.toggle('closed');
+});
