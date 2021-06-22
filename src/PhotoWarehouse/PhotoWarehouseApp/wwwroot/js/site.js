c17795591278601ae8d7f47689f31d54f471b620
Dmitry Matin <super.dmitry320@yandex.ru>
2021-06-22 20:27:23 +0300

    Place the dropdown menu underneath the categories button

--- src/PhotoWarehouse/PhotoWarehouseApp/wwwroot/js/site.js
+++ src/PhotoWarehouse/PhotoWarehouseApp/wwwroot/js/site.js
@@ -1,5 +1,18 @@
 ﻿const menuToggle = document.querySelector('#categoriesMenuToggle');
+const menu = document.querySelector('#expMenu');
+
 menuToggle.addEventListener('click', () => {
-    const menu = document.querySelector('#expMenu');
+    const menuToggleCoords = menuToggle.getBoundingClientRect();
+    console.dir(menuToggleCoords);
+    const x = menuToggleCoords.left;
+    const y = menuToggleCoords.bottom;
+
+
+    menu.style.left = `${x}px`;
+    menu.style.top = `${y}px`;
     menu.classList.toggle('closed');
+});
+
+window.addEventListener('resize', () => {
+    menu.classList.add('closed');
 });
