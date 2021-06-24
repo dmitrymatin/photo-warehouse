2a2773ab993422d33df33568925469ff64246bb2
Dmitry Matin <super.dmitry320@yandex.ru>
2021-06-24 22:20:13 +0300

    Partially implement basket editing

--- src/PhotoWarehouse/PhotoWarehouseApp/Pages/Photos/Basket/Index.cshtml.cs
+++ src/PhotoWarehouse/PhotoWarehouseApp/Pages/Photos/Basket/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -101,6 +102,12 @@ namespace PhotoWarehouseApp.Pages.Photos.Basket
             }
         }
 
+        public class PostData
+        {
+            public int ChosenPhotoItemId { get; set; }
+            public PhotoItemStatus PhotoItemStatus { get; set; }
+        }
+
         public async Task<IActionResult> OnPostSaveAsync()
         {
             if (!ModelState.IsValid)
@@ -110,46 +117,117 @@ namespace PhotoWarehouseApp.Pages.Photos.Basket
             }
 
             var postData = Input.Select(x =>
-                new { ChosenPhotoItemId = x.ChosenPhotoItem.Id, x.PhotoItemStatus });
+                new PostData { ChosenPhotoItemId = x.ChosenPhotoItem.Id, PhotoItemStatus = x.PhotoItemStatus });
 
             var user = await userManager.Users
                 .Include(u => u.PhotoItemsInBasket)
                 .FirstOrDefaultAsync(u => u.UserName == User.Identity.Name);
 
-            foreach (var postDataItem in postData)
+            foreach (var existingBasketItem in user.PhotoItemsInBasket.ToList())
             {
-                var matchingPhotoItem = user.PhotoItemsInBasket.FirstOrDefault(pi => pi.Id == postDataItem.ChosenPhotoItemId);
-
-                if (matchingPhotoItem is not null)
+                if (!postData.Any(pd => pd.ChosenPhotoItemId == existingBasketItem.Id))
                 {
-                    if (postDataItem.PhotoItemStatus == PhotoItemStatus.Deleted)
-                    {
-                        user.PhotoItemsInBasket.Remove(matchingPhotoItem);
-                    }
+                    //var itemToDelete = await context.PhotoItems
+                    //   //.AsNoTracking()
+                    //   .FirstOrDefaultAsync(pi => pi.Id == existingBasketItem.Id);
+
+                    user.PhotoItemsInBasket.Remove(existingBasketItem);
+                    //continue;
                 }
                 else
                 {
-                    // ChosenPhotoItem was added as another version of 
-                    var chosenPhotoItem = context.PhotoItems
-                        .AsNoTracking()
-                        .Include(pi => pi.Photo)
-                        .FirstOrDefault(pi => pi.Id == postDataItem.ChosenPhotoItemId);
-                    var chosenPhoto = chosenPhotoItem.Photo;
+                    var postDataItem = postData.FirstOrDefault(pd => pd.ChosenPhotoItemId == existingBasketItem.Id);
 
-                    var relatedPhotoItems = context.PhotoItems
-                        .AsNoTracking()
-                        .Include(pi => pi.Photo)
-                        .Where(pi => pi.PhotoId == chosenPhoto.Id)
-                        .ToList();
-
-                    var photoItemToAdd = relatedPhotoItems.FirstOrDefault(pi => pi.Id == postDataItem.ChosenPhotoItemId);
-                    if (photoItemToAdd is not null)
+                    if (postDataItem.PhotoItemStatus == PhotoItemStatus.Deleted)
                     {
-                        user.PhotoItemsInBasket.Add(photoItemToAdd);
-                        user.PhotoItemsInBasket.Remove(matchingPhotoItem);
+                        user.PhotoItemsInBasket.Remove(existingBasketItem);
                     }
+
+
+                    //if (basketItemsFromRequest.PhotoItemStatus == PhotoItemStatus.Deleted)
+                    //{
+
+                    //}
+
+                    //var itemToDelete = await context.PhotoItems
+                    //   //.AsNoTracking()
+                    //   .FirstOrDefaultAsync(pi => pi.Id == basketItemsFromRequest.ChosenPhotoItemId);
+
+                    //user.PhotoItemsInBasket.Remove(itemToDelete);
                 }
             }
+
+            foreach (var postDataItem in postData)
+            {
+                if (!user.PhotoItemsInBasket.Any(pi => pi.Id == postDataItem.ChosenPhotoItemId))
+                {
+                    var itemToAdd = context.PhotoItems.FirstOrDefault(pi => pi.Id == postDataItem.ChosenPhotoItemId);
+
+                    user.PhotoItemsInBasket.Add(itemToAdd);
+                }
+            }
+
+            //foreach (var basketItemsFromRequest in postData)
+            //{
+            //    if (!user.PhotoItemsInBasket.Any(pi => pi.Id == basketItemsFromRequest.ChosenPhotoItemId))
+            //    {
+            //        var newItem = await context.PhotoItems
+            //           //.AsNoTracking()
+            //           .FirstOrDefaultAsync(pi => pi.Id == basketItemsFromRequest.ChosenPhotoItemId);
+
+            //        user.PhotoItemsInBasket.Add(newItem);
+            //    }
+            //    if (basketItemsFromRequest.PhotoItemStatus == PhotoItemStatus.Deleted)
+            //    {
+            //        var itemToDelete = await context.PhotoItems
+            //           //.AsNoTracking()
+            //           .FirstOrDefaultAsync(pi => pi.Id == basketItemsFromRequest.ChosenPhotoItemId);
+
+            //        user.PhotoItemsInBasket.Remove(itemToDelete);
+            //    }
+            //}
+
+            //user.PhotoItemsInBasket.Select(pi => postData.Any(new { ChosenPhotoItemId, PhotoItemStatus.Unmodified }));
+
+            //foreach (var userExistingBasketItem in user.PhotoItemsInBasket ?? Enumerable.Empty<PhotoItem>())
+            //{
+            //    if (postData.Any(new { ChosenPhotoItemId = 1, PhotoItemStatus.Deleted })) ;
+            //}
+
+            //foreach (var postDataItem in postData)
+            //{
+            //    var matchingPhotoItem = user.PhotoItemsInBasket.FirstOrDefault(pi => pi.Id == postDataItem.ChosenPhotoItemId);
+
+            //    if (matchingPhotoItem is not null)
+            //    {
+            //        if (postDataItem.PhotoItemStatus == PhotoItemStatus.Deleted)
+            //        {
+            //            user.PhotoItemsInBasket.Remove(matchingPhotoItem);
+            //        }
+            //    }
+            //    else
+            //    {
+            //        // ChosenPhotoItem was added as another version of 
+            //        var chosenPhotoItem = context.PhotoItems
+            //            .AsNoTracking()
+            //            .Include(pi => pi.Photo)
+            //            .FirstOrDefault(pi => pi.Id == postDataItem.ChosenPhotoItemId);
+            //        var chosenPhoto = chosenPhotoItem.Photo;
+
+            //        var relatedPhotoItems = context.PhotoItems
+            //            .AsNoTracking()
+            //            .Include(pi => pi.Photo)
+            //            .Where(pi => pi.PhotoId == chosenPhoto.Id)
+            //            .ToList();
+
+            //        var photoItemToAdd = relatedPhotoItems.FirstOrDefault(pi => pi.Id == postDataItem.ChosenPhotoItemId);
+            //        if (photoItemToAdd is not null)
+            //        {
+            //            user.PhotoItemsInBasket.Add(photoItemToAdd);
+            //            user.PhotoItemsInBasket.Remove(matchingPhotoItem);
+            //        }
+            //    }
+            //}
 
             context.SaveChanges();
 
