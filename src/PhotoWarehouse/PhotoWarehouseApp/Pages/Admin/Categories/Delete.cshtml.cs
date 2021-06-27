697f575b9f651366858bfd5375e1f82f007871f4
Dmitry Matin <super.dmitry320@yandex.ru>
2021-06-27 12:52:04 +0300

    Refactor Admin/Categories/Delete page

--- src/PhotoWarehouse/PhotoWarehouseApp/Pages/Admin/Categories/Delete.cshtml.cs
+++ src/PhotoWarehouse/PhotoWarehouseApp/Pages/Admin/Categories/Delete.cshtml.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using PhotoWarehouse.Data;
 using PhotoWarehouse.Domain.Photos;
+using System.Threading.Tasks;
 
 namespace PhotoWarehouseApp.Pages.Admin.Categories
 {
     public class DeleteModel : PageModel
     {
-        private readonly PhotoWarehouse.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
-        public DeleteModel(PhotoWarehouse.Data.ApplicationDbContext context)
+        public DeleteModel(ApplicationDbContext context)
         {
             _context = context;
         }
